823f464752283f207d9cb3a8021fbc7887fc1cda
BenjiOdhis <nehemiahbenjamin7@gmail.com>
2025-09-30 12:42:04 +0300

    added set theory and validation

--- lite_hangman.py
+++ lite_hangman.py
@@ -1,13 +1,30 @@
-import nltk
+import nltk#add hints,hint counter...hints cost you lives
 from nltk.corpus import words
 import random
-def check_letter(letter,cword,display):
-    for i in range (len(cword)):
-        if letter.lower()==cword[i].lower():
-            display[i]=letter
 
-    print(" ".join(display))
-    return display
+def check_letter(letter,cword,display,guessedset):
+    alphabets=set("abcdefghijklmnopqrstuvwxyz")
+    if letter in guessedset:
+        print("You have already guessed that letter")
+        
+        invalid=1
+    elif letter not in alphabets:
+        print("Enter a letter in the alphabet")
+        
+        invalid=1
+    else:
+        for i in range (len(cword)):
+         if letter.lower()==cword[i].lower():
+            display[i]=letter
+            guessedset.add(letter)
+        else:
+            guessedset.add(letter)
+        
+        invalid=0
+
+        print(" ".join(display))
+        print(guessedset)
+    return display,invalid
 
 
 word_store =words.words()
@@ -20,15 +37,19 @@ display=["_"]*word_length
 wrong=0
 guesses=0
 lives=6
+guessed_letters=set()
+
+
 print("Welcome to Word Guess!")
 print(f"The word has {word_length} letters: ")
 while wrong <6:
     guesses+=1
     display_checker="".join(display)
-    display=check_letter(input("Guess a letter: "),computer_word,display)
+    #tuple unpacking
+    display,invalid=check_letter(input("Guess a letter: "),computer_word,display,guessed_letters)
     if display_checker=="".join(display):
-        wrong+=1
-        lives-=1
+        wrong=wrong+1-invalid
+        lives=lives-1+invalid
         print(f"Not in the word.{lives } lives remaining...")
 
     else:
